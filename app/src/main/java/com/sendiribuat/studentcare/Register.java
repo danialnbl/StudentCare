59ea93e21daf79faa45b904f355b26fc01060b3e
Danial <danialnabil456@gmail.com>
2022-03-09 01:22:18 +0800

    -Login redesign to simpler design for prototype -Added register page and validation

--- app/src/main/java/com/sendiribuat/studentcare/Register.java
+++ app/src/main/java/com/sendiribuat/studentcare/Register.java
@@ -2,13 +2,93 @@ package com.sendiribuat.studentcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Register extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+    private TextView appTitle;
+    private EditText editTextFullName, editTextAge, editTextEmail,  editTextPassword;
+    private Button registerUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        appTitle = (TextView) findViewById(R.id.appTitle);
+        appTitle.setOnClickListener(this);
+        Button registerUser = findViewById(R.id.registerButton);
+        registerUser.setOnClickListener(this);
+
+        editTextFullName = (EditText) findViewById(R.id.fullName);
+        editTextAge = (EditText) findViewById(R.id.age);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.appTitle:
+                startActivity(new Intent(this, login.class));
+                break;
+            case R.id.registerButton:
+                registerUser();
+        }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String fullName = editTextFullName.getText().toString().trim();
+        String age = editTextAge.getText().toString().trim();
+
+        if(fullName.isEmpty()){
+            editTextFullName.setError("Full name is required");
+            editTextFullName.requestFocus();
+            return;
+        }
+
+        if(age.isEmpty()){
+            editTextAge.setError("Age is required");
+            editTextAge.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please provide valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            editTextPassword.setError("Min password length should be 6 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
     }
 }
--- app/src/main/java/com/sendiribuat/studentcare/login.java
+++ app/src/main/java/com/sendiribuat/studentcare/login.java
@@ -2,46 +2,36 @@ package com.sendiribuat.studentcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class login extends AppCompatActivity {
+public class login extends AppCompatActivity implements View.OnClickListener{
 
-    // create object of DatabaseReference class to access firebase's Realtime Database
-    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().getDatabase().getReferenceFromUrl("https://studentcare-95458-default-rtdb.firebaseio.com/");
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText email = findViewById(R.id.loginemail);
-        final EditText password = findViewById(R.id.loginpassword);
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
 
-        final Button loginBtn = findViewById(R.id.loginbutton);
-
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // get data from edittext into string variables
-                final String emailTxt = email.getText().toString();
-                final String passwordTxt = password.getText().toString();
-
-                // check if user fill all the fields
-                if (emailTxt.isEmpty() || passwordTxt.isEmpty()) {
-                    Toast.makeText(login.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
-                }
-
-
-
-            }
-        });
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, Register.class));
+                break;
+        }
     }
 }
