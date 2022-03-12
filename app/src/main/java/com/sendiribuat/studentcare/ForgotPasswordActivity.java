5e0fd1f398fcf7588d42dfe2f8f8897105dc9954
Danial <danialnabil456@gmail.com>
2022-03-12 15:32:46 +0800

    2 type of login and starting forgot password

--- app/src/main/java/com/sendiribuat/studentcare/ForgotPasswordActivity.java
+++ app/src/main/java/com/sendiribuat/studentcare/ForgotPasswordActivity.java
@@ -2,15 +2,13 @@ package com.sendiribuat.studentcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_forgot_password);
     }
 }
---
+++ app/src/main/java/com/sendiribuat/studentcare/WelcomeActivity.java
@@ -0,0 +1,37 @@
+package com.sendiribuat.studentcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button studentBtn, counselorBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        studentBtn = (Button) findViewById(R.id.ToStudentButton);
+        studentBtn.setOnClickListener(this);
+        counselorBtn = (Button) findViewById(R.id.ToCounselorButton);
+        counselorBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.ToStudentButton:
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+
+            case R.id.ToCounselorButton:
+                startActivity(new Intent(this, CounselorLoginActivity.class));
+                break;
+        }
+    }
+}
